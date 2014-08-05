7635427c1b8a23393eff44521db2c730e548b56d
linclark <lin.w.clark@gmail.com>
2014-08-04 23:01:15 -0400

    Add selection sort solution.

--- exercises/sorting_selection_sort/solution/solution.js
+++ exercises/sorting_selection_sort/solution/solution.js
@@ -1,1 +1,23 @@
-// code stuff here
+function SelectionSort() { }
+
+SelectionSort.prototype.sort = function(items) {
+    var n = items.length;
+    for (var i = 0; i < n; i++) {
+        for (var j = i+1; j < n; j++) {
+            var min = i;
+
+            if (items[j] < items[min]) {
+                min = j;
+                this.exchange(items, i, min);
+            }
+        }
+    }
+};
+
+SelectionSort.prototype.exchange = function(items, firstIndex, secondIndex) {
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+};
+
+module.exports = SelectionSort;
